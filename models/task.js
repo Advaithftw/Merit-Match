cb5354709e7c1b4120cea0a190dd01408634b8b5
Advaithftw <advaith.t20@gmail.com>
2024-07-26 23:43:45 +0530

    initial commit

--- models/task.js
+++ models/task.js
@@ -6,7 +6,8 @@ const taskSchema = new mongoose.Schema({
     points:{type: Number,required:true},
     status: { type: String, default: 'pending' },
     createdAt: { type: Date, default: Date.now },
-    postedBy: { type: String, required: true } 
+    postedBy: { type: String, required: true },
+    reservedBy: { type: String, default: null } 
 });
 
 module.exports = mongoose.model('Task', taskSchema);
